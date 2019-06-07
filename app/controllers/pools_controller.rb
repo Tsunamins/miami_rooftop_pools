1408ec6647c6eb5448c447228b1167e36b87538d
Tsunamins <reillyamr@gmail.com>
2019-06-07 15:40:47 -0400

    make page for viewing user info and posts

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -1,2 +1,10 @@
 class PoolController < ApplicationController
+    get '/pools/new_pool' do
+        user = Helpers.current_user(session)
+        if user.nil?
+          redirect to '/login'
+        else
+          erb :'pools/new_pool'
+        end
+    end
 end 
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,47 @@
 class UserController < ApplicationController 
 
     get '/signup' do 
+        if Helpers.is_logged_in?(session)
+            redirect to '/pools'
+          end
         erb :'/users/new_user'
     end 
+
+    post '/signup' do 
+        if Helpers.is_logged_in?(session)
+            redirect to '/pools'
+          elsif !(params.has_value?(""))
+            @user = User.create(params)
+            session["user_id"] = @user.id
+           
+            redirect to '/user_home'
+          else
+            redirect to '/signup'
+          end
+        end
+
+    get '/login' do 
+        if Helpers.is_logged_in?(session)
+            redirect to '/user_home'
+          end
+          erb :'/users/login'
+    end
+
+    post '/login' do
+        @user = User.find_by(:username => params[:username])
+            if @user && @user.authenticate(params[:password])
+            session["user_id"] = @user.id
+            redirect to "/user_home"
+          else
+            redirect to "/login"
+          end
+    end
+
+    get '/user_home' do 
+        erb :'users/show_user_home'
+    end 
+
+
+
 
 end 

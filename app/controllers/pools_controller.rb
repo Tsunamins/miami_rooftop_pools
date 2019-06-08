db61444a757d8ce432331e5dc898e3416ac3043d
Tsunamins <reillyamr@gmail.com>
2019-06-08 10:49:29 -0400

    add post for posting and showing  pools
    needs work

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -6,5 +6,31 @@ class PoolController < ApplicationController
         else
           erb :'pools/new_pool'
         end
+
     end
+
+    post '/pool' do
+      user = Helpers.current_user(session)
+    if user.nil?
+      redirect to '/login'
+    elsif params[:pool][:pool_name].empty?
+      redirect to '/pools/new_pool'
+    else 
+      @temporary = params  
+      @temporary2 = params[:pool][:id]
+      user.save
+      erb :'pools/show_pool'
+    end
+  end 
+
+    get '/pools/:id' do
+      redirect to '/login' unless Helpers.is_logged_in?(session)
+      @pool = Pool.find(params[:id])
+  
+      erb :'/pools/show_pool'
+    end
+    
+  
+
+
 end 

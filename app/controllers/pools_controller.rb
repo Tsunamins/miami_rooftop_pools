a4886caa9a9089a8660ebd8f66372a86f60e5f5a
Tsunamins <reillyamr@gmail.com>
2019-06-09 12:04:26 -0400

    match pool with user_id get user data in /pools/:id

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -1,4 +1,13 @@
 class PoolController < ApplicationController
+  get '/pools' do
+    if Helpers.is_logged_in?(session)
+      @pools = Pool.all
+      erb :'/pools/pools'
+    else
+      redirect to '/users/login'
+    end
+  end
+  
     get '/pools/new_pool' do
         user = Helpers.current_user(session)
         if user.nil?
@@ -15,17 +24,21 @@ class PoolController < ApplicationController
       redirect to '/login'
     elsif params[:pool][:pool_name].empty?
       redirect to '/pools/new_pool'
-    else 
-      @temporary = params  
-      @temporary2 = params[:pool][:id]
-      user.save
-      erb :'pools/show_pool'
-    end
+    else
+      @pool = Pool.create(params['pool'])
+      @pool[:user_id] = user.id
+      @pool.save #is this necessary?   
+      
+      #user.pools.build(params['pool']) #just for notes         
+    end  
+      redirect to "/pools/#{@pool.id}"      
+    
   end 
 
     get '/pools/:id' do
       redirect to '/login' unless Helpers.is_logged_in?(session)
       @pool = Pool.find(params[:id])
+      @user = Helpers.current_user(session)
   
       erb :'/pools/show_pool'
     end

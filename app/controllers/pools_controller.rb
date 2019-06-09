6659cb0e408ba4dd594807cba07ffb5c507b29a6
Tsunamins <reillyamr@gmail.com>
2019-06-09 13:48:40 -0400

    add edit and delete
    however having some problems with deleting and editing certain posts/pools

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -42,6 +42,48 @@ class PoolController < ApplicationController
   
       erb :'/pools/show_pool'
     end
+
+    get '/pools/:id/edit' do
+      redirect to '/login' unless Helpers.is_logged_in?(session)
+  
+      @pool = Pool.find(params[:id])
+      if @pool.user == Helpers.current_user(session)
+          erb :'/pools/edit_pool'
+      else
+        redirect to '/login'
+      end
+    end
+
+    patch '/pools/:id' do
+      @pool = Pool.find(params[:id])
+  
+      if params[:pool][:pool_name].empty?
+        redirect to "/pools/#{@pool.id}/edit"
+      end
+  
+      @pool.update(params[:pool])
+      @pool.save
+      redirect to "/pools/#{@pool.id}"
+  
+    end 
+
+    delete '/pools/:id/delete' do
+      if Helpers.is_logged_in?(session)
+        @pool = Pool.find(params[:id])
+        if @pool.user == Helpers.current_user(session)
+          @pool = Pool.find_by_id(params[:id])
+          @pool.delete
+          redirect to '/user_home'
+          
+        else
+          redirect to '/user_home'
+        end
+      else
+        redirect to '/login'
+      end
+    end
+
+  
     
   
 
